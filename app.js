f4e1371e7dbf57ec2d9f4f71c77f8ea0cfa9ab25
nouhaila <nohaila.chkhonty@gmail.com>
2024-11-29 14:51:25 +0100

    commit

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
